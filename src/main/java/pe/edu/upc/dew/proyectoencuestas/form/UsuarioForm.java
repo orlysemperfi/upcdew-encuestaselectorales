1cfde854521d427842c6bdf9a2b536c8c7efb9a1
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-13 16:11:48 +0000

    Se agrega form para logeo.

---
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/form/UsuarioForm.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.proyectoencuestas.form;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+/**
+ *
+ * @author cramirez
+ */
+public class UsuarioForm extends org.apache.struts.action.ActionForm {
+    
+    private String username;
+    private String password;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+  
+
+    /**
+     *
+     */
+    public UsuarioForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        if (getUsername() == null || getUsername().length() < 1) {
+            errors.add("name", new ActionMessage("error.name.required"));
+            // TODO: add 'error.name.required' key to your resources
+        }
+        return errors;
+    }
+}
