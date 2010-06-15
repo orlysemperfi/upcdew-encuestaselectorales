ac2c1c4c79a61c2e564ec821b7a52580a9a60593
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-15 05:15:19 +0000

    modificacion de nombre la clase segun standar    ----- Jose Flores

--- trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/EncuestaDao.java
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/EncuestaDao.java
@@ -7,15 +7,13 @@ package pe.edu.upc.dew.proyectoencuestas.dao;
 
 import java.util.List;
 import pe.edu.upc.dew.proyectoencuestas.model.dto.Encuesta;
-import pe.edu.upc.dew.proyectoencuestas.model.dto.Pregunta;
 
 /**
  *
- * @author cramirez
+ * @author Owner
  */
 public interface EncuestaDao {
 
-   List<Encuesta> getEncuestasPorDistritos(String distrito);
-   List<Pregunta> getPreguntasPorEncuesta(Integer idEncuesta);
-
+    List<Encuesta> getEncuestasPorDistritos(String distrito);
 }
+
