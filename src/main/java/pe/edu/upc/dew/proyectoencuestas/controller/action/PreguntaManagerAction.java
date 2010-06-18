2de3aba23f3637a59ec5c6a2336964fc782624ce
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-18 06:33:55 +0000

    Modifica PreguntaManagerAction------------------Ceci.

--- src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/PreguntaManagerAction.java
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/PreguntaManagerAction.java
@@ -6,36 +6,63 @@
 package pe.edu.upc.dew.proyectoencuestas.controller.action;
 
 
-import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.struts.actions.DispatchAction;
 import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import pe.edu.upc.dew.proyectoencuestas.form.PreguntaManagerForm;
 import pe.edu.upc.dew.proyectoencuestas.model.dto.Pregunta;
 import pe.edu.upc.dew.proyectoencuestas.service.bo.PreguntaService;
 import pe.edu.upc.dew.proyectoencuestas.service.bo.PreguntaServiceImpl;
-
 
 /**
  *
  * @author Owner
  */
-public class PreguntaManagerAction extends DispatchAction {
+public class PreguntaManagerAction extends org.apache.struts.action.Action {
 
-      public ActionForward aceptar(ActionMapping mapping, ActionForm  form,
+
+    private PreguntaService preguntaService;
+    private static final String SUCCESS = "exito";
+    private static final String ERROR = "error";
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
 
+        this.preguntaService = new PreguntaServiceImpl();
 
-        PreguntaService preguntaBo= new PreguntaServiceImpl();
-        ArrayList<Pregunta> preguntas = preguntaBo.ObtenerListadoPreguntas();
-        System.out.println("hola como estaas"+preguntas.size());
-        request.setAttribute("usuarios", preguntas);
+        int idEncuesta = ((PreguntaManagerForm)form).getNumber();
+      
+        List<Pregunta> preguntas = preguntaService.getPreguntasPorEncuesta(idEncuesta);
 
-        return mapping.findForward("exito");
+            request.setAttribute("preguntas", preguntas);
+
+            if(preguntas.size() > 0)
+            {
+                return mapping.findForward(SUCCESS);
+
+            } else
+            {
+                return mapping.findForward(ERROR);
+            }
+
     }
+
+//      public ActionForward aceptar(ActionMapping mapping, ActionForm  form,
+//            HttpServletRequest request, HttpServletResponse response)
+//            throws Exception {
+//
+//
+//        PreguntaService preguntaBo= new PreguntaServiceImpl();
+//        ArrayList<Pregunta> preguntas = preguntaBo.ObtenerListadoPreguntas();
+//        System.out.println("hola como estaas"+preguntas.size());
+//        request.setAttribute("usuarios", preguntas);
+//
+//        return mapping.findForward("exito");
+//    }
 
 }
