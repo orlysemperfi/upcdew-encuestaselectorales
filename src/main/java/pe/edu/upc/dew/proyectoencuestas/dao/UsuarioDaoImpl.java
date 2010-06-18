abcd1225df305cefb4e2228d93cf355033e56527
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-18 02:52:16 +0000

    UsuarioDaoImpl para acceso a login sin conexión BD.------- Ceci.

--- src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/UsuarioDaoImpl.java
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/UsuarioDaoImpl.java
@@ -23,59 +23,59 @@ public class UsuarioDaoImpl implements UsuarioDao{
 
      public Usuario getUsuarioPorUsername(String username) {
          
-        Usuario usuario = null;
+        Usuario usuario = new Usuario();
 
-//        if (username.equals("admin"))
-//        {
-//            usuario.setIdUsuario(1);
-//            usuario.setNombre("Carlos");
-//            usuario.setApellido("Torres");
-//            usuario.setDni("01234567");
-//            usuario.setRol(1);
-//            usuario.setUsername("admin");
-//            usuario.setPassword("admin");
-//        }
-//        else
-//        {
-//            usuario.setIdUsuario(2);
-//            usuario.setNombre("Ana");
-//            usuario.setApellido("Lopez");
-//            usuario.setDni("12098364");
-//            usuario.setRol(2);
-//            usuario.setUsername("ana");
-//            usuario.setPassword("ana");
-//
-//        }
-        Connection connection = null;
-        Statement st = null;
-        ResultSet rs = null;
-       
-
-        try {
-            connection = MySqlDBConn.getConnection();
-            st = connection.createStatement();
-            rs = st.executeQuery("select cod_usu, nom_usu, ape_usu, nro_doc_usu, rol_usu, CodigoDistrito from tb_usuario where nom_usu='" + username + "'");
-            if (rs.next()) {
-                usuario = new Usuario();
-                usuario.setIdUsuario(rs.getString("cod_usu"));
-                usuario.setNombre(rs.getString("nom_usu"));
-                usuario.setApellido(rs.getString("ape_usu"));
-                usuario.setDni(rs.getString("nro_doc_usu"));
-                usuario.setRol(rs.getInt("rol_usu"));
-                Ubigeo ubigeo = new Ubigeo();
-                ubigeo.setCodDistrito("CodigoDistrito");
-                usuario.setUbigeo(ubigeo);
-//                usuario.setUsername(rs.getString("USERNAME"));
-//                usuario.setPassword(rs.getString("USERNAME"));
-              
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-//            ConnectionUtils.closeResultSet(rs);
-//            ConnectionUtils.closeStatement(st);
-//            ConnectionUtils.closeConnection(connection);
+        if (username.equals("admin"))
+        {
+            usuario.setIdUsuario("1");
+            usuario.setNombre("Carlos");
+            usuario.setApellido("Torres");
+            usuario.setDni("01234567");
+            usuario.setRol(1);
+            usuario.setUsername("admin");
+            usuario.setPassword("admin");
         }
+        else
+        {
+            usuario.setIdUsuario("2");
+            usuario.setNombre("Ana");
+            usuario.setApellido("Lopez");
+            usuario.setDni("12098364");
+            usuario.setRol(2);
+            usuario.setUsername("ana");
+            usuario.setPassword("ana");
+
+        }
+//        Connection connection = null;
+//        Statement st = null;
+//        ResultSet rs = null;
+//
+//
+//        try {
+//            connection = MySqlDBConn.getConnection();
+//            st = connection.createStatement();
+//            rs = st.executeQuery("select cod_usu, nom_usu, ape_usu, nro_doc_usu, rol_usu, CodigoDistrito from tb_usuario where nom_usu='" + username + "'");
+//            if (rs.next()) {
+//                usuario = new Usuario();
+//                usuario.setIdUsuario(rs.getString("cod_usu"));
+//                usuario.setNombre(rs.getString("nom_usu"));
+//                usuario.setApellido(rs.getString("ape_usu"));
+//                usuario.setDni(rs.getString("nro_doc_usu"));
+//                usuario.setRol(rs.getInt("rol_usu"));
+//                Ubigeo ubigeo = new Ubigeo();
+//                ubigeo.setCodDistrito("CodigoDistrito");
+//                usuario.setUbigeo(ubigeo);
+////                usuario.setUsername(rs.getString("USERNAME"));
+////                usuario.setPassword(rs.getString("USERNAME"));
+//
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+////            ConnectionUtils.closeResultSet(rs);
+////            ConnectionUtils.closeStatement(st);
+////            ConnectionUtils.closeConnection(connection);
+//        }
         return usuario;
     }
 
