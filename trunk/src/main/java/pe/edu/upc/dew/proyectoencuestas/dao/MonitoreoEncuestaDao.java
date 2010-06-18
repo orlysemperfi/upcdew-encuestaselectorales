e3c085ed121cc63c46b82ed784c2710c284e72b4
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-18 04:58:06 +0000

    dto de monitoreo(Resultado encuesta final) - gab

---
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/MonitoreoEncuestaDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.proyectoencuestas.dao;
+
+import java.util.List;
+import pe.edu.upc.dew.proyectoencuestas.model.dto.Encuesta;
+/**
+ *
+ * @author Gaby
+ */
+public class MonitoreoEncuestaDao {
+
+
+}
