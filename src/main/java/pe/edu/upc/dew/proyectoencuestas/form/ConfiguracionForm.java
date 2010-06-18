dc5fb348e32595ea5999353ac7a44e2e5d65294b
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-18 05:33:42 +0000

    Creación de la clase ConfiguracionForm   ----- Jose Flores

---
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/form/ConfiguracionForm.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.proyectoencuestas.form;
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ *
+ * @author Owner
+ */
+public class ConfiguracionForm extends ActionForm {
+
+}
