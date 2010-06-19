46b6a67831b88a3ac0cbef6c610b4c7b19105213
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-19 13:19:37 +0000

    Creación de la clase ReporteForm-------------------------------- SOPHIA MENDEZ

---
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/form/ReporteForm.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.proyectoencuestas.form;
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ *
+ * @author Owner
+ */
+public class ReporteForm  extends ActionForm{
+ private String idReporte;
+ private String descripcion;
+
+    public String getIdEncuesta() {
+        return idEncuesta;
+    }
+
+    public void setIdEncuesta(String idEncuesta) {
+        this.idEncuesta = idEncuesta;
+    }
+ private String idEncuesta;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIdReporte() {
+        return idReporte;
+    }
+
+    public void setIdReporte(String idReporte) {
+        this.idReporte = idReporte;
+    }
+
+}
