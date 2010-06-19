48d31e54fc3dab804df12dd656977b86c5009dba
ceci.e.ramirezg@gmail.com <ceci.e.ramirezg@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-19 11:42:49 +0000

    actualizacion de la clase EncuestaForm--------------------------- ----- Jose Flores

--- src/main/java/pe/edu/upc/dew/proyectoencuestas/form/EncuestaForm.java
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/form/EncuestaForm.java
@@ -17,9 +17,71 @@ import org.apache.struts.action.ActionForm;
  * @author 2424005
  */
 public class EncuestaForm extends ActionForm {
-    
+
+
+    private Integer idEncuesta;
+    private String nombre;
+    private String fechaInicio;
+    private String fechaFin;
+    private Integer muestra;
+    private String estado;
+
+
     private String name;
     private String number;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+    
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Integer getIdEncuesta() {
+        return idEncuesta;
+    }
+
+    public void setIdEncuesta(Integer idEncuesta) {
+        this.idEncuesta = idEncuesta;
+    }
+
+    public Integer getMuestra() {
+        return muestra;
+    }
+
+    public void setMuestra(Integer muestra) {
+        this.muestra = muestra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
 
     /**
      * @return
