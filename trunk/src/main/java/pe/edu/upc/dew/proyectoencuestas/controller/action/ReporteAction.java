733ed93ddf7aa6bde786f512e3950914b402d3b0
josefloresingaruca@gmail.com <josefloresingaruca@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-06-29 04:02:47 +0000

    Pruebas con reportes

--- trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/ReporteAction.java
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/ReporteAction.java
@@ -12,6 +12,11 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 import pe.edu.upc.dew.proyectoencuestas.form.ReporteForm;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -23,6 +28,26 @@ public class ReporteAction  extends DispatchAction{
       public ActionForward iniciar(ActionMapping mapping, ActionForm  form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+
+
+   	//Crear el dataset...
+          DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+          dataset.addValue(1.0, "Fila 1", "Columna 1");
+          dataset.addValue(5.0, "Fila 1", "Columna 2");
+          dataset.addValue(3.0, "Fila 1", "Columna 3");
+          dataset.addValue(2.0, "Fila 2", "Columna 1");
+          dataset.addValue(3.0, "Fila 2", "Columna 2");
+          dataset.addValue(2.0, "Fila 2", "Columna 3");
+            //Crear el gráfico...
+          JFreeChart chart = ChartFactory.createBarChart("Mi gráfico","Periodos","Unidades",dataset, PlotOrientation.VERTICAL,true,true,false);
+
+          //crear y visualizar una ventana...
+          ChartFrame frame = new ChartFrame("First", chart);
+          frame.pack();
+          frame.setVisible(true);
+
+
+
              ReporteForm reporteForm = (ReporteForm)form;
 
              request.setAttribute("encuestaDes", reporteForm.getDescripcion());
