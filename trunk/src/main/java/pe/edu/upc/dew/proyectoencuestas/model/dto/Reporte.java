a280401b43dc6b83521823da5a6271c9701a3659
sophiapaola@gmail.com <sophiapaola@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-07-27 23:48:42 +0000

    CreACIÓN DE LA CLASE Reporte -------------------Sophia

---
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/model/dto/Reporte.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.proyectoencuestas.model.dto;
+
+/**
+ *
+ * @author Owner
+ */
+public class Reporte {
+
+    private String cantidad;
+    private String descripcion;
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    
+}
