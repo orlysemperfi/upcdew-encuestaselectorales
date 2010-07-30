6ee97790c99666f84ef2056587d7bf8e7378ed7e
sophia.paola@gmail.com <sophia.paola@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-07-30 07:31:52 +0000

    actualizacion de clase reporte Service implementacion ----Sophia

--- trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/service/bo/ReporteServiceImpl.java
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/service/bo/ReporteServiceImpl.java
@@ -5,10 +5,28 @@
 
 package pe.edu.upc.dew.proyectoencuestas.service.bo;
 
+import java.util.List;
+import pe.edu.upc.dew.proyectoencuestas.dao.ReporteDao;
+import pe.edu.upc.dew.proyectoencuestas.dao.ReporteDaoImpl;
+import pe.edu.upc.dew.proyectoencuestas.model.dto.Reporte;
+
 /**
  *
  * @author Sofía
  */
-public class ReporteServiceImpl {
+public class ReporteServiceImpl implements ReporteService {
+
+    private  ReporteDao reporteDao=null;
+
+    public ReporteServiceImpl(){
+
+    reporteDao = new ReporteDaoImpl();
+
+    }
+
+   public  List<Reporte> getCantidadVotosxCandidato(String encuesta){
+     return  reporteDao.getCantidadVotosxCandidato(encuesta);
+   }
+
 
 }
