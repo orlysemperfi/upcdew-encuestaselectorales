bbe1ffa9788b3b152a14698f798e1ab6a665ec88
josefloresingaruca@gmail.com <josefloresingaruca@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-08-03 22:07:47 +0000

    Creación de métodos DAO IMPLEMENTS  para los graficos -------- Jose Flores

--- trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/ReporteDaoImpl.java
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/ReporteDaoImpl.java
@@ -11,6 +11,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import pe.edu.upc.dew.proyectoencuestas.model.dto.Reporte;
+import pe.edu.upc.dew.proyectoencuestas.model.dto.ReporteEncuesta;
 import pe.edu.upc.dew.proyectoencuestas.util.MySqlDBConn;
 
 /**
@@ -20,7 +21,58 @@ import pe.edu.upc.dew.proyectoencuestas.util.MySqlDBConn;
 public class ReporteDaoImpl implements ReporteDao {
 
 
-       public List<Reporte> getCantidadVotosxCandidato(String encuesta)
+    public List<ReporteEncuesta> getListarPreguntasEncuesta(String idEncuesta)
+     {
+           List<ReporteEncuesta> listaVotos = new ArrayList<ReporteEncuesta>();
+
+		Connection conn = null;
+		Statement stm = null;
+		ResultSet rs = null;
+
+                ReporteEncuesta reporteEncuesta =null;
+
+		// Preparar consulta
+		String sql = " select   e.id_enc,p.id_pre,e.tit_enc,p.des_pre  "+
+                             " from  tb_pregunta p , tb_prexenc pe ,tb_encuesta e   "+
+                             " where  p.id_pre=pe.id_pre  and   pe.id_enc=e.id_enc   and e.id_enc =" + idEncuesta;
+
+
+		// Ejecutar consulta
+		try {
+			conn = MySqlDBConn.getConnection();
+			stm = conn.createStatement();
+			rs = stm.executeQuery(sql);
+
+			// obtener la lista
+			while(rs.next()){
+
+                            reporteEncuesta = new ReporteEncuesta();
+                            reporteEncuesta.setIdEncuesta(rs.getString(1));
+                            reporteEncuesta.setIdPregunta(rs.getString(2));
+                            reporteEncuesta.setDesEncuesta(rs.getString(3));
+                            reporteEncuesta.setDesPregunta(rs.getString(4));
+                            reporteEncuesta.setResultado(getCantidadVotos(idEncuesta,reporteEncuesta.getIdPregunta()));
+
+                              System.out.println("/n tamanio "+reporteEncuesta.getResultado().size());
+             
+                            listaVotos.add(reporteEncuesta);
+
+			}
+		} catch (Exception e) {
+                        e.printStackTrace();
+                }
+                finally {
+			MySqlDBConn.closeResultSet(rs);
+                        MySqlDBConn.closeStatement(stm);
+                        MySqlDBConn.closeConnection(conn);
+		}
+
+
+         return listaVotos;
+     }
+
+
+       public List<Reporte> getCantidadVotos(String idEncuesta,String idPregunta)
      {
            List<Reporte> listaVotos = new ArrayList<Reporte>();
 
@@ -33,12 +85,10 @@ public class ReporteDaoImpl implements ReporteDao {
 		// Preparar consulta
 		String sql =" select count(r.id_opc)as total,r.id_pre,des_pre,r.id_opc,des_opc "+  
                             " from tb_pregunta p, tb_opcion o,tb_result_fin r "+ 
-                            " where r.id_enc=1    and r.id_pre=1 "+ 
+                            " where r.id_enc="+ idEncuesta +"  and r.id_pre="+ idPregunta +" "+
                             " and p.id_pre=r.id_pre and o.id_opc=r.id_opc "+ 
                             " group by r.id_enc,r.id_opc,r.id_pre "+ 
                             " order by id_pre,des_pre ";
-
-
 
 		// Ejecutar consulta
 		try {
