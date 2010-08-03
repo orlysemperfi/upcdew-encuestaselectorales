21fd8d7bd5bbd49cb078ad259e37c0b65128ff02
josefloresingaruca@gmail.com <josefloresingaruca@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-08-03 22:08:51 +0000

    Creación de métodos ReporteService IMPLEMENTS  para los graficos -------- Jose Flores

--- src/main/java/pe/edu/upc/dew/proyectoencuestas/service/bo/ReporteServiceImpl.java
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/service/bo/ReporteServiceImpl.java
@@ -9,7 +9,8 @@ import java.util.List;
 import pe.edu.upc.dew.proyectoencuestas.dao.ReporteDao;
 import pe.edu.upc.dew.proyectoencuestas.dao.ReporteDaoImpl;
 import pe.edu.upc.dew.proyectoencuestas.model.dto.Reporte;
-
+import pe.edu.upc.dew.proyectoencuestas.model.dto.ReporteEncuesta;
+import pe.edu.upc.dew.proyectoencuestas.util.GeneradorGraficos;
 /**
  *
  * @author Sofía
@@ -24,9 +25,23 @@ public class ReporteServiceImpl implements ReporteService {
 
     }
 
-   public  List<Reporte> getCantidadVotosxCandidato(String encuesta){
-     return  reporteDao.getCantidadVotosxCandidato(encuesta);
+   public  List<Reporte> getCantidadVotos(String encuesta,String idPregunta){
+     return  reporteDao.getCantidadVotos(encuesta,idPregunta);
    }
 
+     public List<ReporteEncuesta> getListarPreguntasEncuesta(String encuesta,String ruta)
+  {
+
+      List<ReporteEncuesta> reporte = reporteDao.getListarPreguntasEncuesta(encuesta);
+
+      GeneradorGraficos genera= new GeneradorGraficos();
+      for(int x =0 ; x<reporte.size();x++){
+
+        reporte.get(x).setRutaReporte(genera.PieChart3D( reporte.get(x).getResultado(), ruta+ reporte.get(x).getIdEncuesta()+reporte.get(x).getIdPregunta()));
+      System.out.println("miraaaaaaaaaa"+reporte.get(x).getRutaReporte());
+      }
+
+     return  reporte;
+   }
 
 }
