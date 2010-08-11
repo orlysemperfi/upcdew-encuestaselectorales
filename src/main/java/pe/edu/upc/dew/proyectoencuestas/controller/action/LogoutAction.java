35469512dc6a87540091a94a311e553fc443bb89
josefloresingaruca@gmail.com <josefloresingaruca@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-08-11 23:02:59 +0000

    clase para cerrar sesion----- Jose Flores

---
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/LogoutAction.java
@@ -0,0 +1,39 @@
+package pe.edu.upc.dew.proyectoencuestas.controller.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+
+ 
+public class LogoutAction extends DispatchAction {
+	
+	public ActionForward execute(
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response)
+			throws Exception {
+
+
+		HttpSession session = request.getSession(true);
+
+		try {
+			if (session != null) {
+				session.removeAttribute("usuario");
+                       
+
+			}
+			request.getSession().invalidate();
+		} catch (Exception e) {
+
+
+		}
+
+		return mapping.findForward("exito");
+	}
+}
