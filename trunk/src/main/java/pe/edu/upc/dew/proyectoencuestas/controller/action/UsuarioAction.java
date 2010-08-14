ad37eb450920bde4a18ed10a8274dd63e01c9ee6
josefloresingaruca@gmail.com <josefloresingaruca@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-08-14 01:35:49 +0000

    modificacion  -------------------Jose

--- trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/UsuarioAction.java
+++ trunk/src/main/java/pe/edu/upc/dew/proyectoencuestas/controller/action/UsuarioAction.java
@@ -19,56 +19,47 @@ import pe.edu.upc.dew.proyectoencuestas.service.bo.EncuestaServiceImpl;
 import pe.edu.upc.dew.proyectoencuestas.service.bo.UsuarioService;
 import pe.edu.upc.dew.proyectoencuestas.service.bo.UsuarioServiceImpl;
 
-
-
 public class UsuarioAction extends org.apache.struts.action.Action {
 
-     private UsuarioService usuarioService;
-     private EncuestaService encuestaService;
+    private UsuarioService usuarioService;
+    private EncuestaService encuestaService;
 
     /* forward name="success" path="" */
     private static final String SUCCESS = "exito";
     private static final String ERROR = "error";
     private static final String USERSUCCESS = "userexito";
 
-
-
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
 
-        this.usuarioService = new UsuarioServiceImpl();
-        this.encuestaService = new EncuestaServiceImpl();
+        try {
+            this.usuarioService = new UsuarioServiceImpl();
+            this.encuestaService = new EncuestaServiceImpl();
 
-        String username = ((UsuarioForm)form).getUsername().trim();
-        String password = ((UsuarioForm)form).getPassword().trim();
+            String username = ((UsuarioForm) form).getUsername().trim();
+            String password = ((UsuarioForm) form).getPassword().trim();
+            String login = usuarioService.obtenerLoginUsuario(username);
+            System.out.println("loginn " + login);
 
+            if (!login.equals("0")) {
+                String contrasena = usuarioService.obtenerContrasenaUsuario(login);
 
-        String login=usuarioService.obtenerLoginUsuario(username);
+                System.out.println("contrasena " + contrasena);
 
-        System.out.println("loginn "+login);
+                if (contrasena.equals(password)) {
+                    Usuario usuario = usuarioService.getUsuarioPorUsername(username, password);
 
+                    System.out.println("usuario " + usuario.getNombre());
 
-     try {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("usuario", usuario);
 
-        if(!login.equals("0")){
-           String contrasena=usuarioService.obtenerContrasenaUsuario(login);
-
-           System.out.println("contrasena "+contrasena);
-
-           if(contrasena.equals(password)){
-               Usuario usuario = usuarioService.getUsuarioPorUsername(username, password);
-
-                   System.out.println("usuario "+usuario.getNombre());
-
-                   HttpSession session = request.getSession();
-                   session.setAttribute("usuario", usuario);
-
-                    if(usuario.getRol() == 1) {
+                    if (usuario.getRol() == 1) {
                         this.encuestaService = new EncuestaServiceImpl();
                         List<Encuesta> encuestas = encuestaService.getEncuestas();
 
-                        if (encuestas.size() > 0){
+                        if (encuestas.size() > 0) {
                             request.setAttribute("encuestas", encuestas);
                             return mapping.findForward(SUCCESS);
 
@@ -76,12 +67,12 @@ public class UsuarioAction extends org.apache.struts.action.Action {
                             return mapping.findForward(ERROR);
                         }
 
-                 }else{
+                    } else {
                         List<Encuesta> encuestas = encuestaService.getEncuestasPorDistritos(usuario.getUbigeo().getCodDistrito());
-                        if (encuestas == null)  {
+                        if (encuestas == null) {
                             return mapping.findForward(ERROR);
-                        }else{
-                            if (encuestas.size() > 0){
+                        } else {
+                            if (encuestas.size() > 0) {
                                 request.setAttribute("encuestas", encuestas);
                                 return mapping.findForward(USERSUCCESS);
                             } else {
@@ -90,23 +81,18 @@ public class UsuarioAction extends org.apache.struts.action.Action {
                         }
                     }
 
+                } else {
+                    request.setAttribute("mensaje", "Password incorrecto");
+
+                }
+
             } else {
-               request.setAttribute("mensaje","Password incorrecto");
-
+                request.setAttribute("mensaje", "Usuario incorrecto");
             }
+        } catch (Exception e) {
+            request.setAttribute("mensaje", e.getMessage());
 
-           }else{
-                 request.setAttribute("mensaje","Usuario incorrecto");
-           }
-
-
-          } catch (Exception e) {
-
-                System.out.println("mesnajeeee "+e.getMessage());
-
-               
         }
-
         return mapping.findForward(ERROR);
 
     }
