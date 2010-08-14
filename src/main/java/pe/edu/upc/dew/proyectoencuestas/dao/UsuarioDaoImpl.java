98b37d478b511cb18b79db15cb8e082cb044038f
josefloresingaruca@gmail.com <josefloresingaruca@gmail.com@6d3de8f4-d2ba-a87d-34df-e172685b56b0>
2010-08-14 01:35:41 +0000

    modificacion  -------------------Jose

--- src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/UsuarioDaoImpl.java
+++ src/main/java/pe/edu/upc/dew/proyectoencuestas/dao/UsuarioDaoImpl.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.edu.upc.dew.proyectoencuestas.dao;
 
 import pe.edu.upc.dew.proyectoencuestas.model.dto.Usuario;
@@ -14,15 +13,14 @@ import java.sql.SQLException;
 import pe.edu.upc.dew.proyectoencuestas.model.dto.Ubigeo;
 import pe.edu.upc.dew.proyectoencuestas.util.MySqlDBConn;
 
-
 /**
  *
  * @author cramirez
  */
-public class UsuarioDaoImpl implements UsuarioDao{
+public class UsuarioDaoImpl implements UsuarioDao {
 
-     public Usuario getUsuarioPorUsername(String username, String password) {
-        
+    public Usuario getUsuarioPorUsername(String username, String password) {
+        System.out.println("UsuarioDaoImpl.getUsuarioPorUsername");
         Usuario usuario = null;
 //    
         Connection connection = null;
@@ -32,14 +30,14 @@ public class UsuarioDaoImpl implements UsuarioDao{
         try {
             connection = MySqlDBConn.getConnection();
             st = connection.createStatement();
-            rs = st.executeQuery("select cod_usu, nom_usu, ape_usu, nro_doc_usu, rol_usu, CodigoDistrito, user_usu, pass_usu from tb_usuario where user_usu='" + username + "' and pass_usu='"+ password +"'");
+            rs = st.executeQuery("select cod_usu, nom_usu, ape_usu, nro_doc_usu, rol_usu, CodigoDistrito, user_usu, pass_usu from tb_usuario where user_usu='" + username + "' and pass_usu='" + password + "'");
             if (rs.next()) {
 
                 usuario = new Usuario();
                 usuario.setIdUsuario(rs.getString("cod_usu"));
                 usuario.setNombre(rs.getString("nom_usu"));
                 usuario.setApellido(rs.getString("ape_usu"));
-                usuario.setDni(rs.getString("nro_doc_usu"));                  
+                usuario.setDni(rs.getString("nro_doc_usu"));
                 usuario.setRol(rs.getInt("rol_usu"));
                 Ubigeo ubigeo = new Ubigeo();
                 ubigeo.setCodDistrito(rs.getString("CodigoDistrito"));
@@ -49,12 +47,9 @@ public class UsuarioDaoImpl implements UsuarioDao{
 
             }
         } catch (SQLException e) {
-
-                 throw new IllegalStateException("Error al obtener el usuario", e);
-          		
-        }
-        finally {
-           
+            System.out.println("SQLException");
+            throw new IllegalStateException("Error al obtener el usuario", e);
+        } finally {
             MySqlDBConn.closeResultSet(rs);
             MySqlDBConn.closeStatement(st);
             MySqlDBConn.closeConnection(connection);
@@ -62,10 +57,9 @@ public class UsuarioDaoImpl implements UsuarioDao{
         return usuario;
     }
 
+    public String obtenerLoginUsuario(String sLogin) {
 
-      public String obtenerLoginUsuario(String sLogin) {
-
-        String codigo="0";
+        String codigo = "0";
         Connection connection = null;
         Statement st = null;
         ResultSet rs = null;
@@ -75,13 +69,12 @@ public class UsuarioDaoImpl implements UsuarioDao{
             st = connection.createStatement();
             rs = st.executeQuery("select  user_usu from tb_usuario where user_usu='" + sLogin + "'");
             while (rs.next()) {
-              codigo = rs.getString("user_usu");
+                codigo = rs.getString("user_usu");
             }
         } catch (SQLException e) {
-             throw new IllegalStateException("Error al obtener el usuario",e);
-        }
-        finally {
-
+            System.out.println("SQLException");
+            throw new IllegalStateException("Error al obtener el usuario", e);
+        } finally {
             MySqlDBConn.closeResultSet(rs);
             MySqlDBConn.closeStatement(st);
             MySqlDBConn.closeConnection(connection);
@@ -89,10 +82,9 @@ public class UsuarioDaoImpl implements UsuarioDao{
         return codigo;
     }
 
+    public String obtenerContrasenaUsuario(String sLogin) {
 
-      public String obtenerContrasenaUsuario(String sLogin) {
-
-        String contrasena="0";
+        String contrasena = "0";
         Connection connection = null;
         Statement st = null;
         ResultSet rs = null;
@@ -102,12 +94,11 @@ public class UsuarioDaoImpl implements UsuarioDao{
             st = connection.createStatement();
             rs = st.executeQuery("select  pass_usu from tb_usuario where user_usu='" + sLogin + "'");
             while (rs.next()) {
-              contrasena = rs.getString("pass_usu");
+                contrasena = rs.getString("pass_usu");
             }
         } catch (SQLException e) {
-             throw new IllegalStateException("Error al obtener el usuario",e);
-        }
-        finally {
+            throw new IllegalStateException("Error al obtener el usuario", e);
+        } finally {
 
             MySqlDBConn.closeResultSet(rs);
             MySqlDBConn.closeStatement(st);
@@ -115,7 +106,4 @@ public class UsuarioDaoImpl implements UsuarioDao{
         }
         return contrasena;
     }
-
-
-
 }
